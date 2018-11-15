f3554369488492873ecdfef0b537e90e46fe74a7
AlbinoB <albinobraganza21@gmail.com>
2018-11-15 08:29:25 +0530

    moved all fns to stack_array_str.h header file//final 1.1

--- stack_array_string.cpp
+++ stack_array_string.cpp
@@ -1,12 +1,8 @@
 #include<iostream>
+#include<stack_array_str.h>
 #include<string.h>
 using namespace std;
-	int push(char *,int);
-	int pop(char *,int);
-	void display(char *,int);
-	char display_top(char *,int);
-	int isfull(int ,int);
-	int isempty(int);
+
 int main()
 {
 
@@ -57,57 +53,4 @@ int main()
 	    cout<<"\n___________________________________________________\n";
 	}while(c!=0 && c<5 && c>0);
 	return 0;
-}
-
-	int push(char *arrstring,int top)
-	{
-	    char ch;
-	    cout<<"\n Enter the element to be pushed to stack:";
-        cin>>ch;
-        arrstring[++top]=ch;
-        return top;
-	}
-int pop(char *arrstring,int top){
-    cout<<"\n The element popped is:"<<arrstring[top--];
-    return top;
-}
-
-	void display(char *arrstring,int top)
-		{
-		    if(top==-1){
-                cout<<"\n stack is empty";
-		    }else
-		    {
-                cout<<"\n top "<<"\n  :\n";
-                for(int i=top;i>=0;i--)
-                {
-                    cout<<"| "<<arrstring[i]<<" |"<<"\n";
-                }
-		    }
-		}
-
-    char display_top(char *arrstring,int top){
-        return arrstring[top];
-    }
-
-int isfull(int top,int arrsize){
-    if((top+1)==arrsize)//top+2 as the last position is holding '/0'
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-int isempty(int top){
-    if(top==-1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
 }
